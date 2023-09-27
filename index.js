70959e2ac69be311ec85c972d5421ca55ca73601
mohammedasimahmed <asim60999@gmail.com>
2023-09-28 01:24:21 +0530

    fixed video for whe client disconnects

--- index.js
+++ index.js
@@ -13,19 +13,26 @@ const io = require("socket.io")(server,{
     }
 })
 
+const socketToRooms = new Map()
+
 io.on("connection",(socket)=>{
     console.log("Connected with client")
     socket.on("join_room",(room)=>{
         socket.join(room)
+        socketToRooms.set(socket.id,room)
         socket.to(room).emit("new_user",socket.id)
         console.log("joined room "+room)
     })
     socket.on("leave_room",(room)=>{
         socket.leave(room)
+        socket.to(socketToRooms.get(socket.id)).emit("user_left",socket.id)
+        socketToRooms.delete(socket.id)
     })
-    socket.on("disconnect",()=>{
-        socket
-    })
+    socket.on("disconnect", () => {
+        console.log(socket.id + " left");     
+        socket.to(socketToRooms.get(socket.id)).emit("user_left",socket.id)
+        socketToRooms.delete(socket.id)
+    });
 })
 
 server.listen(5000,()=>console.log("Server started at port 5000"))
